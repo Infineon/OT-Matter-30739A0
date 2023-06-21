024c91fa0fd7792589468d1b41b38da13f36a9ac
C.H.Cheng <chao-hsiang.cheng@infineon.com>
2023-06-22 01:39:27 +0800

    BTSDK-9331 Add API and update firmware to build 309
    update firmware tag: CYW20739B2_002.003.026.0309.0000, commit ddd30f4c7a4fd521dbe9d8f434c181410bbb049b
    create new crypto wrappers for hw ccm, sha256, pka, and sw rom aes

--- COMPONENT_30739A0/include/hal/wiced_hal_crypto.h
+++ COMPONENT_30739A0/include/hal/wiced_hal_crypto.h
@@ -50,10 +50,26 @@
 /** @brief wiced crypto result */
 typedef enum
 {
+    WICED_CRYPTO_CCM_INPUT_ERROR = 0,
+    WICED_CRYPTO_CCM_AUTH_FAIL = 1,
+    WICED_CRYPTO_CCM_SUCCESS = 2,
+    WICED_CRYPTO_SHA256_INPUT_ERROR = 3,
+    WICED_CRYPTO_SHA256_FAIL = 4,
+    WICED_CRYPTO_SHA256_SUCCESS = 5,
+    WICED_CRYPTO_MULMOD_FAIL = 6,
+    WICED_CRYPTO_MULMOD_SUCCESS = 7,
     WICED_CRYPTO_ECC_POINT_MUL_CFG_COMPLETE = 11,    //HW_ECC_POINT_MUL_CFG_COMPLETE
     WICED_CRYPTO_ECC_POINT_MUL_SUCCESS = 12,         //HW_ECC_POINT_MUL_SUCCESS
     WICED_CRYPTO_ECC_POINT_MUL_FAIL = 13,            //HW_ECC_POINT_MUL_FAIL
+    WICED_CRYPTO_GENERAL_ALLOC_FAIL = 14,
 }wiced_crypto_result_t;
+
+/** @brief wiced ccm mode: encrypt or decrypt */
+typedef enum
+{
+    WICED_CRYPTO_CCM_DECRYPT = 0,
+    WICED_CRYPTO_CCM_ENCRYPT = 1
+}wiced_crypto_ccm_mode_t;
 
 //==================================================================================================
 // Type Definitions
@@ -85,6 +101,15 @@ typedef struct wiced_crypto_ecc_point_mul
     wiced_crypto_ecc_point_mul_callback callback;	//callback function
 }wiced_crypto_ecc_point_mul_t;
 
+/*
+ *   The structure of AES ECB context
+ */
+typedef struct wiced_crypto_aes_context
+{
+    uint8_t ksch[240];
+    uint8_t rnd;
+} wiced_crypto_aes_context_t;
+
 //==================================================================================================
 // Functions
 //==================================================================================================
@@ -100,5 +125,123 @@ typedef struct wiced_crypto_ecc_point_mul
  *                  WICED_CRYPTO_ECC_POINT_MUL_FAIL          : fail
  */
 wiced_crypto_result_t wiced_hal_crypto_ecc_point_mul(wiced_crypto_ecc_point_mul_t *p_ecc_pt_mul);
+
+/*
+ * Function         wiced_hal_crypto_ccm_engine
+ *
+ * Run CCM (Counter with Cipher Block Chaining-Message Authentication Code) with HW acceleration
+ *
+ * @param[in]       mode    : select ccm encrypt or decrypt
+ *
+ * @param[in]       key     : buffer of the ccm key to encrypt or decrypt data
+ *
+ * @param[in]       key_len : ccm key length (bytes)
+ *
+ * @param[in]       iv      : buffer of the initialization vector (nonce)
+ *
+ * @param[in]       iv_len  : initialization vector length (bytes), limited to 11-14
+ *
+ * @param[in]       add     : buffer of additional authenticated data
+ *
+ * @param[in]       add_len : additional authenticated data length (bytes), limited to 0-2047
+ *
+ * @param[in]       input   : buffer of ccm input text
+ *
+ * @param[in]       length  : input text length (bytes), limited to 0-2047
+ *
+ * @param[out]      tag     : buffer of an output holding calculated authentication tag
+ *
+ * @param[in]       tag_len : tag length (bytes), limited to 0, 4, 8, 12, 16
+ *
+ * @param[out]      output  : buffer of output text
+ *
+ * @param[in]       ccm_buf : wiced ccm function needs caller creating a buffer with size (add_len + tag_len + length + 1)
+ *                            to use. Caller needs free this buffer after wiced ccm function done.
+ *
+ * @return          WICED_CRYPTO_CCM_INPUT_ERROR  : bad input
+ *                  WICED_CRYPTO_CCM_OUT_OF_MEMROY: no memory for HW accelerator operation
+ *                  WICED_CRYPTO_CCM_AUTH_FAIL    : HW accelerator fail
+ *                  WICED_CRYPTO_CCM_SUCCESS      : success
+ */
+wiced_crypto_result_t wiced_hal_crypto_ccm_engine(wiced_crypto_ccm_mode_t mode, const uint8_t *key, uint8_t key_len,
+        const uint8_t *iv, uint8_t iv_len, const uint8_t *add, uint8_t add_len, const uint8_t *input, uint16_t length,
+        uint8_t *tag, uint8_t tag_len, uint8_t *output, uint8_t *ccm_buf);
+
+/*
+ * Function         wiced_hal_crypto_aes_setkey
+ *
+ * Store the aes encrypt/decrypt key with this aes context
+ *
+ * @param[in]       ctx     : aes context
+ *
+ * @param[in]       key     : buffer of the aes key to encrypt or decrypt data
+ *
+ * @param[in]       key_len : aes key length (bytes)
+ */
+void wiced_hal_crypto_aes_setkey(wiced_crypto_aes_context_t *ctx, const uint8_t *key, uint8_t key_len);
+
+/*
+ * Function         wiced_hal_crypto_aes_encrypt
+ *
+ * Run AES-ECB encryption
+ *
+ * @param[in]       ctx     : aes context
+ *
+ * @param[in]       input   : buffer of input text (plaintext)
+ *
+ * @param[out]      output  : buffer of output text (ciphertext)
+ */
+void wiced_hal_crypto_aes_encrypt(wiced_crypto_aes_context_t *ctx, const uint8_t *input, uint8_t *output);
+
+/*
+ * Function         wiced_hal_crypto_aes_decrypt
+ *
+ * Run AES-ECB decryption
+ *
+ * @param[in]       ctx     : aes context
+ *
+ * @param[in]       input   : buffer of input text (ciphertext)
+ *
+ * @param[out]      output  : buffer of output text (plaintext)
+ */
+void wiced_hal_crypto_aes_decrypt(wiced_crypto_aes_context_t *ctx, const uint8_t *input, uint8_t *output);
+
+/*
+ * Function         wiced_hal_crypto_sha256_engine
+ *
+ * Run SHA-256 with HW acceleration to get 32-byte hash value
+ *
+ * @param[in]       input   : buffer of input text
+ *
+ * @param[out]      output  : buffer of output text
+ *
+ * @param[in]       length  : input text length (bytes)
+ *
+ * @return          WICED_CRYPTO_SHA256_INPUT_ERROR: bad input
+ *                  WICED_CRYPTO_SHA256_FAIL       : HW accelerator fail
+ *                  WICED_CRYPTO_SHA256_SUCCESS    : success
+ */
+wiced_crypto_result_t wiced_hal_crypto_sha256_engine(const uint8_t *input, uint8_t *output, uint16_t length);
+
+/*
+ * Function         wiced_hal_crypto_pka_mulmod
+ *
+ * Run big number calculation with HW acceleration. result = (left * right) % div
+ *
+ * @param[out]      result  : buffer of final remainder value
+ *
+ * @param[in]       left    : buffer of multiplier1
+ *
+ * @param[in]       right   : buffer of multiplier2
+ *
+ * @param[in]       div     : buffer of divider
+ *
+ * @param[in]       word_size : word(4-byte) length of multipliers, divider, and remainder
+ *
+ * @return          WICED_CRYPTO_MULMOD_FAIL   : HW accelerator fail
+ *                  WICED_CRYPTO_MULMOD_SUCCESS: success
+ */
+wiced_crypto_result_t wiced_hal_crypto_pka_mulmod(uint32_t *result, const uint32_t *left, const uint32_t *right,
+        const uint32_t *div, uint16_t word_size);
 
 #endif //_WICED_HAL_CRYPTO_H_
